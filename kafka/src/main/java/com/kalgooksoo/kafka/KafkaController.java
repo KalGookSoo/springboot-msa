024334a20d61368dca8593b74029383ef4cc7d28
김도엽 <miro3721@plani.co.kr>
2024-02-16 14:48:52 +0900

    feat: 모듈 디렉토리 세팅

---
+++ kafka/src/main/java/com/kalgooksoo/kafka/KafkaController.java
@@ -0,0 +1,21 @@
+package com.kalgooksoo.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/publish")
+    public void publishMessage(@RequestBody String message) {
+        kafkaTemplate.send("my-topic", message);
+    }
+}
