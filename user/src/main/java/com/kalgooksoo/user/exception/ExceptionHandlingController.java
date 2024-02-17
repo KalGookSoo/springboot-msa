c3c27033e6bf3c2a4be37aa83a7fbdf7913d87b7
김도엽 <miro3721@plani.co.kr>
2024-02-17 13:30:28 +0900

    style: 미사용 코드 제거

--- user/src/main/java/com/kalgooksoo/user/exception/ExceptionHandlingController.java
+++ user/src/main/java/com/kalgooksoo/user/exception/ExceptionHandlingController.java
@@ -1,13 +1,10 @@
 package com.kalgooksoo.user.exception;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.stream.Collectors;
 
 /**
  * 예외 처리를 담당하는 컨트롤러 어드바이스 클래스입니다.
