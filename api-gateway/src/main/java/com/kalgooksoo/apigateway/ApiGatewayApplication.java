ae97c73a94e92b2cbe1bf349d2abe7d87cc6f5e6
김도엽 <ga.miro3721@gmail.com>
2024-02-20 18:52:04 +0900

    style: 주석 추가

--- api-gateway/src/main/java/com/kalgooksoo/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/kalgooksoo/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * API 게이트웨이 애플리케이션
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
