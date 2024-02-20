4c95d0052b31ef69a600a4bbe82e1bdd7a145ffa
김도엽 <ga.miro3721@gmail.com>
2024-02-20 18:42:23 +0900

    refactor: authorization 모듈명 auth로 변경 인증 및 인가를 모두 수행하기 위해서 명칭을 좀 더 일반적으로 변경하였습니다.

--- auth/src/main/java/com/kalgooksoo/auth/AuthApplication.java
+++ auth/src/main/java/com/kalgooksoo/auth/AuthApplication.java
@@ -1,18 +1,18 @@
-package com.kalgooksoo.authorization;
+package com.kalgooksoo.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 인증 서비스 애플리케이션
+ * 인증 및 인가 서비스 애플리케이션
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AuthorizationApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 
 }
