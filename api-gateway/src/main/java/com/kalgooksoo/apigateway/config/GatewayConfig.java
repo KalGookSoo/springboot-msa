e31a4711842e5c8d08eb111e93c34ce02cf56ef0
김도엽 <miro3721@plani.co.kr>
2024-03-01 23:05:08 +0900

    feat: api-gateway 검증 로직 수정

--- api-gateway/src/main/java/com/kalgooksoo/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/kalgooksoo/apigateway/config/GatewayConfig.java
@@ -5,23 +5,35 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder, TokenValidationFilter tokenValidationFilter) {
-        return builder.routes()
-                .route("user-service", p -> p
-                        .path("/users/**")
-                        .filters(f -> f.filter(tokenValidationFilter.apply(new TokenValidationFilter.Config())))
-                        .uri("lb://user-service"))
+        RouteLocatorBuilder.Builder routes = builder.routes();
 
-                .route("security-service", p -> p
-                        .path("/auth/**")
-                        .uri("lb://security-service"))
+        routes.route("user-service-sign-up", p -> p
+                .path("/users").and().method(HttpMethod.POST)
+                .uri("lb://user-service"));
 
-                .build();
+        routes.route("user-service-sign-in", p -> p
+                .path("/users/sign-in").and().method(HttpMethod.POST)
+                .uri("lb://user-service"));
+
+        routes.route("user-service", p -> p
+                .path("/users/**")
+                .filters(f -> f.filter(tokenValidationFilter.apply(new TokenValidationFilter.Config())))
+                .uri("lb://user-service")
+        );
+
+        routes.route("security-service", p -> p
+                .path("/auth/**")
+                .uri("lb://security-service"));
+
+        return routes.build();
+
     }
 
 }
--- api-gateway/src/main/java/com/kalgooksoo/apigateway/filter/TokenValidationFilter.java
+++ api-gateway/src/main/java/com/kalgooksoo/apigateway/filter/TokenValidationFilter.java
@@ -1,16 +1,24 @@
 package com.kalgooksoo.apigateway.filter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class TokenValidationFilter extends AbstractGatewayFilterFactory<TokenValidationFilter.Config> {
+
+    private final Logger logger = LoggerFactory.getLogger(TokenValidationFilter.class);
 
     private final WebClient webClient;
 
@@ -23,34 +31,34 @@ public class TokenValidationFilter extends AbstractGatewayFilterFactory<TokenVal
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             String bearerToken = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-            JsonNode jsonNode = webClient.get()
+            return webClient.get()
                     .uri("/auth/token")
                     .header(HttpHeaders.AUTHORIZATION, bearerToken)
-                    .exchangeToMono(clientResponse -> clientResponse.bodyToMono(JsonNode.class))
-                    .block();
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                        logger.error("HttpStatus code: {}", clientResponse.statusCode());
+                        return Mono.error(new RuntimeException("HttpStatus code: " + clientResponse.statusCode()));
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+                        logger.error("HttpStatus code: {}", clientResponse.statusCode());
+                        return Mono.error(new RuntimeException("HttpStatus code: " + clientResponse.statusCode()));
+                    })
+                    .bodyToMono(JsonNode.class)
+                    .doOnError(e -> {
+                        ServerHttpResponse response = exchange.getResponse();
+                        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                        response.setComplete();
+                    })
+                    .then(chain.filter(exchange))
+                    .onErrorResume(e -> Mono.empty());
 
-            if (jsonNode == null) {
-                ServerHttpResponse response = exchange.getResponse();
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
-                return response.setComplete();
-            }
-
-            return chain.filter(exchange);
         };
     }
 
+    @Getter
+    @Setter
     public static class Config {
-
         private boolean valid;
-
-        public boolean isValid() {
-            return valid;
-        }
-
-        public void setValid(boolean valid) {
-            this.valid = valid;
-        }
-
     }
 
 }
