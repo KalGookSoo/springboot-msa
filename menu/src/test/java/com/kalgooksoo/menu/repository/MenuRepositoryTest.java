5f059b3cfb7b94a11e0492537d3a7b6557784e7a
김도엽 <miro3721@plani.co.kr>
2024-03-10 20:25:17 +0900

    test: 예외 처리 테스트 케이스 추가

--- menu/src/test/java/com/kalgooksoo/menu/repository/MenuRepositoryTest.java
+++ menu/src/test/java/com/kalgooksoo/menu/repository/MenuRepositoryTest.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -24,7 +29,7 @@ class MenuRepositoryTest {
     }
 
     @Test
-    @DisplayName("메뉴를 생성합니다.")
+    @DisplayName("메뉴를 저장합니다. 성공 시 메뉴를 반환합니다.")
     void saveTest() {
         // Given
         Menu menu = Menu.create("공지사항", "http://www.kalgooksoo.com/categories/1/articles", null, "anonymous");
@@ -37,7 +42,41 @@ class MenuRepositoryTest {
     }
 
     @Test
-    @DisplayName("메뉴를 조회합니다.")
+    @DisplayName("메뉴를 저장합니다. 실패 시 IllegalArgumentException을 던집니다.")
+    void saveTestWithNull() {
+        // Given
+        Menu menu = null;
+
+        // When & Then
+        assertThrows(IllegalArgumentException.class, () -> menuRepository.save(menu));
+    }
+
+    @Test
+    @DisplayName("모든 메뉴를 조회합니다. 성공 시 메뉴 목록을 반환합니다.")
+    void findAllTest() {
+        // Given
+        Menu menu = Menu.create("공지사항", "http://www.kalgooksoo.com/categories/1/articles", null, "anonymous");
+        Menu savedMenu = menuRepository.save(menu);
+
+        // When
+        List<Menu> menus = menuRepository.findAll();
+
+        // Then
+        assertTrue(menus.stream().anyMatch(m -> m.getId().equals(savedMenu.getId())));
+    }
+
+    @Test
+    @DisplayName("모든 메뉴를 조회합니다. 실패 시 메뉴 빈 목록을 반환합니다.")
+    void findAllTestWithEmpty() {
+        // When
+        List<Menu> menus = menuRepository.findAll();
+
+        // Then
+        assertTrue(menus.isEmpty());
+    }
+
+    @Test
+    @DisplayName("메뉴를 조회합니다. 성공 시 메뉴를 반환합니다.")
     void findByIdTest() {
         // Given
         Menu menu = Menu.create("공지사항", "http://www.kalgooksoo.com/categories/1/articles", null, "anonymous");
@@ -52,24 +91,30 @@ class MenuRepositoryTest {
     }
 
     @Test
-    @DisplayName("메뉴를 수정합니다.")
-    void updateTest() {
+    @DisplayName("메뉴를 조회합니다. 실패 시 실패 시 빈 Optional을 반환합니다.")
+    void findByIdTestWithEmpty() {
         // Given
-        Menu menu = Menu.create("공지사항", "http://www.kalgooksoo.com/categories/1/articles", null, "anonymous");
-        Menu savedMenu = menuRepository.save(menu);
+        String id = UUID.randomUUID().toString();
 
-        // When
-        savedMenu.update("오시는 길", "http://www.kalgooksoo.com/categories/2/articles");
-        Menu updatedMenu = menuRepository.save(savedMenu);
+        // when
+        Optional<Menu> foundMenu = menuRepository.findById(id);
 
         // Then
-        assertNotNull(updatedMenu);
-        assertEquals(savedMenu.getId(), updatedMenu.getId());
-        assertEquals("오시는 길", updatedMenu.getName());
+        assertTrue(foundMenu.isEmpty());
     }
 
     @Test
-    @DisplayName("메뉴를 삭제합니다.")
+    @DisplayName("메뉴를 수정합니다. 없는 메뉴를 수정 시 NoSuchElementException을 던집니다.")
+    void updateTestWithNull() {
+        // Given
+        String id = UUID.randomUUID().toString();
+
+        // When & Then
+        assertThrows(NoSuchElementException.class, () -> menuRepository.findById(id).orElseThrow());
+    }
+
+    @Test
+    @DisplayName("메뉴를 삭제합니다. 성공 시 삭제된 메뉴를 조회할 수 없습니다.")
     void deleteTest() {
         // Given
         Menu menu = Menu.create("공지사항", "http://www.kalgooksoo.com/categories/1/articles", null, "anonymous");
@@ -77,10 +122,30 @@ class MenuRepositoryTest {
 
         // When
         menuRepository.deleteById(savedMenu.getId());
-        Menu deletedMenu = menuRepository.findById(savedMenu.getId()).orElse(null);
+        Optional<Menu> deletedMenu = menuRepository.findById(savedMenu.getId());
 
         // Then
-        assertNull(deletedMenu);
+        assertTrue(deletedMenu.isEmpty());
+    }
+
+    @Test
+    @DisplayName("메뉴를 삭제합니다. 실패 시 IllegalArgumentException을 던집니다.")
+    void deleteTestWithEmpty() {
+        // Given
+        Menu menu = null;
+
+        // When & Then
+        assertThrows(IllegalArgumentException.class, () -> menuRepository.save(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴를 삭제합니다. 없는 메뉴를 삭제 시 NoSuchElementException을 던집니다.")
+    void deleteTestWithNull() {
+        // Given
+        String id = UUID.randomUUID().toString();
+
+        // When & Then
+        assertThrows(NoSuchElementException.class, () -> menuRepository.deleteById(id));
     }
 
 }
