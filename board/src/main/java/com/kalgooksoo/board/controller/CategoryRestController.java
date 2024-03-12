3ee6a3407a6269d19e3b2fd76f241aec5b1ccf01
kimdoyeob <ga.miro3721@gmail.com>
2024-03-12 15:42:02 +0900

    feat: CategoryRestController 추가

--- board/src/main/java/com/kalgooksoo/board/controller/CategoryRestController.java
+++ board/src/main/java/com/kalgooksoo/board/controller/CategoryRestController.java
@@ -1,10 +1,27 @@
 package com.kalgooksoo.board.controller;
 
+import com.kalgooksoo.board.domain.Category;
+import com.kalgooksoo.board.command.CreateCategoryCommand;
+import com.kalgooksoo.board.model.HierarchicalCategory;
+import com.kalgooksoo.board.command.UpdateCategoryCommand;
 import com.kalgooksoo.board.service.CategoryService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * 카테고리 REST 컨트롤러
@@ -16,5 +33,85 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryRestController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<EntityModel<Category>> create(@Valid @RequestBody CreateCategoryCommand command) {
+        Category menu = categoryService.create(command);
+
+        ResponseEntity<EntityModel<Category>> invocationValue = methodOn(this.getClass())
+                .findById(menu.getId());
+
+        Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                .withRel("self");
+
+        EntityModel<Category> entityModel = EntityModel.of(menu, link);
+        return ResponseEntity.status(HttpStatus.CREATED).body(entityModel);
+    }
+
+    @GetMapping
+    public ResponseEntity<CollectionModel<EntityModel<HierarchicalCategory>>> findAll() {
+        List<EntityModel<HierarchicalCategory>> entityModels = categoryService.findAll()
+                .stream()
+                .map(hierarchicalCategory -> {
+                    ResponseEntity<EntityModel<Category>> invocationValue = methodOn(this.getClass())
+                            .findById(hierarchicalCategory.id());
+
+                    Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                            .withRel("self");
+
+                    return EntityModel.of(hierarchicalCategory, link);
+                })
+                .toList();
+
+        ResponseEntity<CollectionModel<EntityModel<HierarchicalCategory>>> invocationValue = methodOn(this.getClass())
+                .findAll();
+
+        Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                .withRel("self");
+
+        CollectionModel<EntityModel<HierarchicalCategory>> collectionModel = CollectionModel.of(entityModels, link);
+        return ResponseEntity.ok(collectionModel);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<Category>> findById(
+            @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id
+    ) {
+        Category menu = categoryService.findById(id).orElseThrow(() -> new NoSuchElementException("카테고리가 존재하지 않습니다"));
+
+        ResponseEntity<CollectionModel<EntityModel<HierarchicalCategory>>> invocationValue = methodOn(this.getClass())
+                .findAll();
+
+        Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                .withRel("self");
+
+        EntityModel<Category> entityModel = EntityModel.of(menu, link);
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<Category>> updateById(
+            @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id,
+            @Valid @RequestBody UpdateCategoryCommand command
+    ) {
+        Category menu = categoryService.update(id, command);
+
+        ResponseEntity<EntityModel<Category>> invocationValue = methodOn(this.getClass())
+                .findById(menu.getId());
+
+        Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                .withRel("self");
+
+        EntityModel<Category> entityModel = EntityModel.of(menu, link);
+        return ResponseEntity.ok(entityModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(
+            @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id
+    ) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- board/src/test/java/com/kalgooksoo/board/controller/CategoryRestControllerTest.java
+++ board/src/test/java/com/kalgooksoo/board/controller/CategoryRestControllerTest.java
@@ -1,21 +1,36 @@
 package com.kalgooksoo.board.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kalgooksoo.board.command.CreateCategoryCommand;
+import com.kalgooksoo.board.command.UpdateCategoryCommand;
+import com.kalgooksoo.board.domain.Category;
+import com.kalgooksoo.board.domain.CategoryType;
 import com.kalgooksoo.board.repository.CategoryJpaRepository;
 import com.kalgooksoo.board.repository.CategoryRepository;
 import com.kalgooksoo.board.service.CategoryService;
 import com.kalgooksoo.board.service.DefaultCategoryService;
 import com.kalgooksoo.exception.ExceptionHandlingController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * 카테고리 REST 컨트롤러 테스트
@@ -40,6 +55,182 @@ class CategoryRestControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(categoryRestController, exceptionHandlingController).build();
     }
 
+    @Test
+    @DisplayName("카테고리를 생성합니다. 성공 시 응답 코드 201을 반환합니다.")
+    void createSouldReturnCreated() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
 
+        // When
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 생성합니다. 실패 시 응답 코드 400을 반환합니다.")
+    void createSouldReturnBadRequest() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, null, CategoryType.PUBLIC.name(), "admin");
+
+        // When
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리 목록을 조회합니다. 성공 시 응답 코드 200을 반환합니다.")
+    void findAllSouldReturnOk() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
+        mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+
+        // When
+        mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 조회합니다. 성공 시 응답 코드 200을 반환합니다.")
+    void findByIdShouldReturnOk() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
+        MockHttpServletResponse httpServletResponse = mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn().getResponse();
+
+        EntityModel<Category> entityModel = mapper.readValue(httpServletResponse.getContentAsString(), new TypeReference<>() {});
+        Category menu = entityModel.getContent();
+        Assertions.assertThat(menu).isNotNull();
+
+        // When
+        mockMvc.perform(get("/categories/{id}", menu.getId()))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 조회합니다. 존재하지 않는 카테고리 조회 시 응답 코드 404를 반환합니다.")
+    void findByIdShouldReturnNotFound() throws Exception {
+        // Given
+
+        // When
+        mockMvc.perform(get("/categories/{id}", UUID.randomUUID()))
+                .andExpect(status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 수정합니다. 성공 시 응답 코드 200을 반환합니다.")
+    void updateShouldReturnOk() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
+        MockHttpServletResponse response = mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse();
+
+        EntityModel<Category> entityModel = mapper.readValue(response.getContentAsString(), new TypeReference<>() {});
+        Category menu = entityModel.getContent();
+        Assertions.assertThat(menu).isNotNull();
+
+        UpdateCategoryCommand updateCategoryCommand = new UpdateCategoryCommand("공지사항 수정", CategoryType.PUBLIC.name());
+
+        // When
+        mockMvc.perform(put("/categories/{id}", menu.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updateCategoryCommand)))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 수정합니다. 실패 시 응답 코드 400을 반환합니다.")
+    void updateShouldReturnBadRequest() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
+        MockHttpServletResponse response = mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse();
+
+        EntityModel<Category> entityModel = mapper.readValue(response.getContentAsString(), new TypeReference<>() {});
+        Category menu = entityModel.getContent();
+        Assertions.assertThat(menu).isNotNull();
+
+        UpdateCategoryCommand updateCategoryCommand = new UpdateCategoryCommand(null, CategoryType.PUBLIC.name());
+
+        // When
+        mockMvc.perform(put("/categories/{id}", menu.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updateCategoryCommand)))
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 수정합니다. 존재하지 않는 카테고리 수정 시 응답 코드 404를 반환합니다.")
+    void updateShouldReturnNotFound() throws Exception {
+        // Given
+        UpdateCategoryCommand updateCategoryCommand = new UpdateCategoryCommand("공지사항 수정", CategoryType.PUBLIC.name());
+
+        // When
+        mockMvc.perform(put("/categories/{id}", UUID.randomUUID())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updateCategoryCommand)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("카테고리를 삭제합니다. 성공 시 응답 코드 204를 반환합니다.")
+    void deleteSouldReturnNoContent() throws Exception {
+        // Given
+        CreateCategoryCommand createCategoryCommand = new CreateCategoryCommand(null, "공지사항", CategoryType.PUBLIC.name(), "admin");
+        MockHttpServletResponse response = mockMvc.perform(post("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(createCategoryCommand)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn()
+                .getResponse();
+
+        EntityModel<Category> entityModel = mapper.readValue(response.getContentAsString(), new TypeReference<>() {});
+        Category menu = entityModel.getContent();
+        Assertions.assertThat(menu).isNotNull();
+
+        // When
+        mockMvc.perform(delete("/categories/{id}", menu.getId()))
+                .andExpect(status().isNoContent())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("카테고리를 삭제합니다. 존재하지 않는 카테고리 삭제 시 응답 코드 404를 반환합니다.")
+    void deleteSouldReturnNotFound() throws Exception {
+        // Given
+
+        // When
+        mockMvc.perform(delete("/categories/{id}", UUID.randomUUID()))
+                .andExpect(status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
 
 }
