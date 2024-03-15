6fda3067ea6ad5708f9ba7b5c5a690b6dff8f00c
kimdoyeob <miro3721@plani.co.kr>
2024-03-15 23:12:47 +0900

    feat: Vote 도메인 추가

---
+++ board/src/main/java/com/kalgooksoo/board/domain/VoteId.java
@@ -0,0 +1,36 @@
+package com.kalgooksoo.board.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import java.io.Serializable;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = PROTECTED)
+@Embeddable
+public class VoteId implements Serializable {
+
+    /**
+     * 참조 식별자
+     */
+    private String referenceId;
+
+    /**
+     * 투표자
+     */
+    private String voter;
+
+    public VoteId(String referenceId, String voter) {
+        Assert.notNull(referenceId, "참조 식별자는 NULL이 될 수 없습니다");
+        Assert.notNull(voter, "투표자는 NULL이 될 수 없습니다");
+        this.referenceId = referenceId;
+        this.voter = voter;
+    }
+
+}
