932e826ac637a8ad83dc5946bcf2f7640c06e000
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 13:25:00 +0900

    feat: OAS 애노테이션 추가

--- user/src/main/java/com/kalgooksoo/user/search/UserSearch.java
+++ user/src/main/java/com/kalgooksoo/user/search/UserSearch.java
@@ -11,22 +11,35 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "계정 검색 조건")
 public class UserSearch extends PageVO {
 
-    @Parameter(description = "계정명(대체키)")
-    @Schema(description = "계정명(대체키)")
+    @Parameter(
+            description = "계정명(대체키)",
+            example = "testuser1",
+            schema = @Schema(description = "계정명(대체키)", implementation = String.class)
+    )
     private String username;
 
-    @Parameter(description = "계정명(또는 별칭)")
-    @Schema(description = "계정명(또는 별칭)")
+    @Parameter(
+            description = "계정명(또는 별칭)",
+            example = "홍길동",
+            schema = @Schema(description = "계정명(또는 별칭)", implementation = String.class)
+    )
     private String name;
 
-    @Parameter(description = "이메일 ID")
-    @Schema(description = "이메일 ID")
+    @Parameter(
+            description = "이메일 ID",
+            example = "testuser",
+            schema = @Schema(description = "이메일 ID", implementation = String.class)
+    )
     private String emailId;
 
-    @Parameter(description = "연락처")
-    @Schema(description = "연락처")
+    @Parameter(
+            description = "연락처",
+            example = "010-1234-5678",
+            schema = @Schema(description = "연락처", implementation = String.class)
+    )
     private String contactNumber;
 
     @JsonIgnore
