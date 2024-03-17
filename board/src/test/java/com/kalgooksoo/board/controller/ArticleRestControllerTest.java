24c70732834ada33305ab9015589a50c1572fc05
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 20:23:39 +0900

    test: ArticleRestControllerTest 생성

---
+++ board/src/test/java/com/kalgooksoo/board/controller/ArticleRestControllerTest.java
@@ -0,0 +1,14 @@
+package com.kalgooksoo.board.controller;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 게시글 REST 컨트롤러 테스트
+ */
+class ArticleRestControllerTest {
+
+    private MockMvc mockMvc;
+
+}
