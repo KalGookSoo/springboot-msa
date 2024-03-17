952adef1bc48be221d785ce58f70d76afa1b730f
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 14:43:56 +0900

    feat: CategoryRestController.move 메서드 추가

--- board/src/main/java/com/kalgooksoo/board/controller/CategoryRestController.java
+++ board/src/main/java/com/kalgooksoo/board/controller/CategoryRestController.java
@@ -1,10 +1,12 @@
 package com.kalgooksoo.board.controller;
 
+import com.kalgooksoo.board.command.MoveCategoryCommand;
 import com.kalgooksoo.board.domain.Category;
 import com.kalgooksoo.board.command.CreateCategoryCommand;
 import com.kalgooksoo.board.model.HierarchicalCategory;
 import com.kalgooksoo.board.command.UpdateCategoryCommand;
 import com.kalgooksoo.board.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,8 +36,11 @@ public class CategoryRestController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "카테고리 생성", description = "카테고리를 생성합니다")
     @PostMapping
-    public ResponseEntity<EntityModel<Category>> create(@Valid @RequestBody CreateCategoryCommand command) {
+    public ResponseEntity<EntityModel<Category>> create(
+            @Parameter(description = "카테고리 생성 명령", schema = @Schema(implementation = CreateCategoryCommand.class)) @Valid @RequestBody CreateCategoryCommand command
+    ) {
         Category menu = categoryService.create(command);
 
         ResponseEntity<EntityModel<Category>> invocationValue = methodOn(this.getClass())
@@ -48,6 +53,7 @@ public class CategoryRestController {
         return ResponseEntity.status(HttpStatus.CREATED).body(entityModel);
     }
 
+    @Operation(summary = "카테고리 목록 조회", description = "카테고리 목록을 조회합니다")
     @GetMapping
     public ResponseEntity<CollectionModel<EntityModel<HierarchicalCategory>>> findAll() {
         List<EntityModel<HierarchicalCategory>> entityModels = categoryService.findAll()
@@ -73,6 +79,7 @@ public class CategoryRestController {
         return ResponseEntity.ok(collectionModel);
     }
 
+    @Operation(summary = "카테고리 조회", description = "카테고리를 조회합니다")
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<Category>> findById(
             @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id
@@ -89,10 +96,11 @@ public class CategoryRestController {
         return ResponseEntity.ok(entityModel);
     }
 
+    @Operation(summary = "카테고리 수정", description = "카테고리를 수정합니다")
     @PutMapping("/{id}")
     public ResponseEntity<EntityModel<Category>> updateById(
             @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id,
-            @Valid @RequestBody UpdateCategoryCommand command
+            @Parameter(description = "카테고리 수정 커맨드", schema = @Schema(implementation = UpdateCategoryCommand.class)) @Valid @RequestBody UpdateCategoryCommand command
     ) {
         Category menu = categoryService.update(id, command);
 
@@ -106,12 +114,31 @@ public class CategoryRestController {
         return ResponseEntity.ok(entityModel);
     }
 
+    @Operation(summary = "카테고리 삭제", description = "카테고리를 삭제합니다")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(
             @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id
     ) {
         categoryService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "카테고리 이동", description = "카테고리를 이동합니다")
+    @PutMapping("/{id}/move")
+    public ResponseEntity<EntityModel<Category>> move(
+            @Parameter(description = "카테고리 식별자", schema = @Schema(type = "string", format = "uuid")) @PathVariable String id,
+            @Parameter(description = "카테고리 이동 커맨드", schema = @Schema(implementation = MoveCategoryCommand.class)) @Valid @RequestBody MoveCategoryCommand command
+    ) {
+        Category category = categoryService.move(id, command);
+
+        ResponseEntity<EntityModel<Category>> invocationValue = methodOn(this.getClass())
+                .findById(category.getId());
+
+        Link link = WebMvcLinkBuilder.linkTo(invocationValue)
+                .withRel("self");
+
+        EntityModel<Category> entityModel = EntityModel.of(category, link);
+        return ResponseEntity.ok(entityModel);
     }
 
 }
