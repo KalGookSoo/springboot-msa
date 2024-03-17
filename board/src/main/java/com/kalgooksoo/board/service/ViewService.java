838e68dbca0f93001eb9f2c43a3bd32f81ea1e7c
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 14:45:04 +0900

    feat: ViewService 생성

---
+++ board/src/main/java/com/kalgooksoo/board/service/ViewService.java
@@ -0,0 +1,7 @@
+package com.kalgooksoo.board.service;
+
+/**
+ * 뷰 서비스
+ */
+public interface ViewService {
+}
---
+++ board/src/main/java/com/kalgooksoo/board/service/VoteService.java
@@ -0,0 +1,7 @@
+package com.kalgooksoo.board.service;
+
+/**
+ * 투표 서비스
+ */
+public interface VoteService {
+}
