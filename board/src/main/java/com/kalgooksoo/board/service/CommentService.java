2da68a2f561258d91a49edba14fbdbefe8b4228c
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 14:44:49 +0900

    feat: CommentService 명세 수정

--- board/src/main/java/com/kalgooksoo/board/service/CommentService.java
+++ board/src/main/java/com/kalgooksoo/board/service/CommentService.java
@@ -1,21 +1,19 @@
 package com.kalgooksoo.board.service;
 
 import com.kalgooksoo.board.command.CreateCommentCommand;
-import com.kalgooksoo.board.command.DislikesCommand;
-import com.kalgooksoo.board.command.LikesCommand;
 import com.kalgooksoo.board.command.UpdateCommentCommand;
 import com.kalgooksoo.board.domain.Comment;
+
+import java.util.List;
 
 public interface CommentService {
 
     Comment create(CreateCommentCommand command);
 
+    List<Comment> findAllByArticleId(String articleId);
+
     Comment update(String id, UpdateCommentCommand command);
 
     void delete(String id);
-
-    void likes(String id, LikesCommand command);
-
-    void dislikes(String id, DislikesCommand command);
 
 }
