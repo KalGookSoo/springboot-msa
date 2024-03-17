936f2d21b809e6cb86977ed692c94db406f1dfd1
kimdoyeob <ga.miro3721@gmail.com>
2024-03-17 19:17:04 +0900

    fix: DI 순환참조 제거

--- user/src/main/java/com/kalgooksoo/user/config/UserApplicationRunner.java
+++ user/src/main/java/com/kalgooksoo/user/config/UserApplicationRunner.java
@@ -3,22 +3,25 @@ package com.kalgooksoo.user.config;
 import com.kalgooksoo.exception.UsernameAlreadyExistsException;
 import com.kalgooksoo.user.command.CreateUserCommand;
 import com.kalgooksoo.user.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@RequiredArgsConstructor
 public class UserApplicationRunner implements CommandLineRunner {
 
     private final Logger logger = LoggerFactory.getLogger(UserApplicationRunner.class);
 
     private final UserService userService;
+
+    public UserApplicationRunner(@Lazy UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public void run(String... args) {
