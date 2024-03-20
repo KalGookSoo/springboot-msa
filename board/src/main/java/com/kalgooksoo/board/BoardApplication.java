88cd1308b262cf1141b33ae48bce22a0595a6ff4
kimdoyeob <ga.miro3721@gmail.com>
2024-03-20 13:48:41 +0900

    feat: Import ExceptionHandlingController

--- board/src/main/java/com/kalgooksoo/board/BoardApplication.java
+++ board/src/main/java/com/kalgooksoo/board/BoardApplication.java
@@ -1,14 +1,17 @@
 package com.kalgooksoo.board;
 
+import com.kalgooksoo.core.exception.ExceptionHandlingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * 게시판 서비스 애플리케이션
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import({ExceptionHandlingController.class})
 public class BoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
--- menu/src/main/java/com/kalgooksoo/menu/MenuApplication.java
+++ menu/src/main/java/com/kalgooksoo/menu/MenuApplication.java
@@ -1,14 +1,17 @@
 package com.kalgooksoo.menu;
 
+import com.kalgooksoo.core.exception.ExceptionHandlingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 /**
  * 메뉴 서비스 애플리케이션
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import({ExceptionHandlingController.class})
 public class MenuApplication {
 
     public static void main(String[] args) {
