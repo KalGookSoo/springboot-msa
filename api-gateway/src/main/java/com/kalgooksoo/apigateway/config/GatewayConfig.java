0c3dcdef3089cdfefaebb7b66a05c88ef463b255
kimdoyeob <ga.miro3721@gmail.com>
2024-03-24 12:23:55 +0900

    refactor: api-gateway route 목록 yaml로 이관

--- api-gateway/src/main/java/com/kalgooksoo/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/kalgooksoo/apigateway/config/GatewayConfig.java
@@ -1,61 +1,12 @@
 package com.kalgooksoo.apigateway.config;
 
 import com.kalgooksoo.apigateway.filter.TokenValidationFilter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GatewayConfig {
-
-    /**
-     * RouteLocator 빈을 생성합니다.
-     * TODO 이 내용을 application.yaml에 정의할 수 있도록 변경해야 함. api-gateway의 재기동 없이 config-server의 refresh만으로 라우트 변경이 가능해야 함.
-     *
-     * @param builder               RouteLocatorBuilder
-     * @param tokenValidationFilter TokenValidationFilter
-     * @return RouteLocator 빈
-     */
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder, TokenValidationFilter tokenValidationFilter) {
-        RouteLocatorBuilder.Builder routes = builder.routes();
-
-        routes.route("user-service-sign-up", p -> p
-                .path("/users").and().method(HttpMethod.POST)
-                .uri("lb://user-service"));
-
-        routes.route("user-service-sign-in", p -> p
-                .path("/users/sign-in").and().method(HttpMethod.POST)
-                .uri("lb://user-service"));
-
-        routes.route("user-service", p -> p
-                .path("/users/**")
-                .filters(f -> f.filter(tokenValidationFilter.apply(new TokenValidationFilter.Config())))
-                .uri("lb://user-service")
-        );
-
-        routes.route("security-service", p -> p
-                .path("/auth/**")
-                .uri("lb://security-service"));
-
-        routes.route("menu-service", p -> p
-                .path("/menus/**")
-                .uri("lb://menu-service"));
-
-        routes.route("board-service", p -> p
-                .path("/boards/**")
-                .uri("lb://board-service"));
-
-        routes.route("acl-service", p -> p
-                .path("/acl/**")
-                .uri("lb://acl-service"));
-
-        return routes.build();
-
-    }
 
     @Bean
     public TokenValidationFilter tokenValidationFilter() {
